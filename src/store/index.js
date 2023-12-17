8af267d53411864d1bb6a5396b8d786cada32127
Amirreza <amirreza.m13825@gmail.com>
2023-12-17 16:14:01 +0330

    store requests with vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from 'vuex';
 import coachesModule from './modules/coaches/index.js';
+import requestesModule from "./modules/requestes/index.js"
 
 const store = createStore({
   modules: {
     coaches: coachesModule,
+    requests: requestesModule,
     state() {
       return {
         userId: 'c3',
