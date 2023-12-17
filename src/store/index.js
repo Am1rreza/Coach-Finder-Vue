37902da0bf7b3239287f7e4189a4571812d8de59
Amirreza <amirreza.m13825@gmail.com>
2023-12-17 16:19:46 +0330

    fix the store bug (userId was undefined)

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,18 @@
 import { createStore } from 'vuex';
 import coachesModule from './modules/coaches/index.js';
-import requestesModule from "./modules/requestes/index.js"
+import requestesModule from './modules/requestes/index.js';
 
 const store = createStore({
+  state: {
+    userId: 'c3',
+  },
   modules: {
     coaches: coachesModule,
     requests: requestesModule,
-    state() {
-      return {
-        userId: 'c3',
-      };
-    },
-    getters: {
-      userId(state) {
-        return state.userId;
-      },
+  },
+  getters: {
+    userId(state) {
+      return state.userId;
     },
   },
 });
