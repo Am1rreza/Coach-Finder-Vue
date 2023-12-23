c506e2b539e30b1b1f78f557b91e0d28748a2710
Amirreza <amirreza.m13825@gmail.com>
2023-12-23 19:12:01 +0330

    not found page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,3 +1,11 @@
 <template>
-  <div></div>
+  <section>
+    <base-card>
+      <h2>Page Not Found!</h2>
+      <p>
+        This page could not be found. Go back to the
+        <router-link to="/">Homepage</router-link>
+      </p>
+    </base-card>
+  </section>
 </template>
